c62a690339524bdcb5894ac8d287764c288a778a
Dayeon-36 <drymkite36@naver.com>
2025-07-16 00:50:08 +0900

    Update my_solution.py

--- my_solution.py
+++ my_solution.py
@@ -1,7 +1,3 @@
-Last login: Sun Jul 13 22:52:52 on ttys000
-(base) kwondayeon@gwondayeon-ui-MacBookAir ~ % python3
-Python 3.12.7 | packaged by Anaconda, Inc. | (main, Oct  4 2024, 08:22:19) [Clang 14.0.6 ] on darwin
-Type "help", "copyright", "credits" or "license" for more information.
 >>> def hello():
 ...     return "Hello"
 ... 
